eccfc6a2d7106fcb0c3a28407070649e6f3d32ce
tcschulz <tanya.schulz@colorado.edu>
2019-04-01 18:15:09 -0600

    test post method

--- main.js
+++ main.js
@@ -1,44 +1,42 @@
-var createError = require('http-errors');
+var http = require("http");
 var express = require('express');
-var path = require('path');
-hike = require('./routes/hike');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
-app.get('/hikes', hike.index);
-app.post('/add_hike', hike.add_hike);
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: true });
+var prompt = require('prompt');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// http://localhost:8082/form 
+// Running Server Details.
+var server = app.listen(8082, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log("Example app listening at %s:%s Port", host, port)
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+ 
+ 
+app.get('/form', function (req, res) {
+  var html='';
+  html +="<body>";
+  html += "<form action='/thank'  method='post' name='form1'>";
+  html += "<b><font size= 25 color=blue>Insanity NFL</font><b></p>";
+  html += "Favorite NFC North Team:</p><input type= 'text' name='team'></p>";
+  html += "Favorite NFC North Player:</p><input type='text' name='player'></p>";
+  html += "Do you like Brett Favre? </p><input type='text' name='favre'></p>";
+  html += "Do you think Aaron Roggers is better than Brett Favre?</p><input type='text' name='arod'></p>";
+  html += "<img src = https://bloximages.chicago2.vip.townnews.com/madison.com/content/tncms/assets/v3/editorial/d/18/d1808247-1af1-5b2a-bd8e-ad93d83d1af8/5b2ec4d7f0e1e.image.jpg?resize=400%2C407 /></p>"
+  html += "<input type='submit' value='Submit'></p>";
+  html += "<INPUT type='reset'  value='Reset'></p>";
+  html += "</form>";
+  html += "</body>";
+  res.send(html);
 });
-
-module.exports = app;
+ 
+app.post('/thank', urlencodedParser, function (req, res){
+  var reply='';
+  reply += "<b>Your favorite team is:</b> " + req.body.team +"</p>";
+  reply += "<b>Your favorite player is:</b> " + req.body.player+"</p>"; 
+  reply += "<b>Do you like Brett Favre?</b> " + req.body.favre+"</p>";
+  reply += "<b>Do you think Aaron Rodgers is better than Brett Favre? </b>" + req.body.arod;
+  res.send(reply);
+ });
